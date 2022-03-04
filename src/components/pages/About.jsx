9d33334e6ca86731aa50734644ae7d725efe5a73
Daniel Jaramillo <cribidany@gmail.com>
2022-03-03 19:49:28 -0500

    Fix some general errors

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,8 +5,7 @@ import DefaultLayout from "../layouts/DefaultLayout";
  * About component.
  * @returns {JSX.Element}
  */
-
-export const about = () => (
+export const About = () => (
   <DefaultLayout>
     <h1>About page</h1>
   </DefaultLayout>
