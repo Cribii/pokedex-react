de94f76ae22b9eecf9f69ad99f8934c1b6c84414
Daniel Jaramillo <cribidany@gmail.com>
2022-03-19 17:58:48 -0500

    Define NavBrand component

--- src/components/organisms/Navigation.jsx
+++ src/components/organisms/Navigation.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import NavBrand from '../molecules/NavBrand';
 
 /**
  * Navigation component.
  * @returns {JSX.Element}
  */
-
-export const Navigation = () => <nav></nav>;
+export const Navigation = () => ( 
+<nav>
+    <NavBrand/>
+</nav>
+);
 
 
 export default Navigation;
